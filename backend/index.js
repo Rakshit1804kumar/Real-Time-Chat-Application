c40dddf0ee9d3bb364417b59626d879ea02a99bd
Rakshit Kumar <147365205+Rakshit1804kumar@users.noreply.github.com>
2026-04-06 22:28:24 +0530

    Update CORS configuration to allow multiple origins

--- backend/index.js
+++ backend/index.js
@@ -13,9 +13,12 @@ const port=process.env.PORT || 5000
 
 
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
+  origin: [
+    "http://localhost:5173",
+    "https://real-time-chat-application-azure-two.vercel.app"
+  ],
+  credentials: true
+}));
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/auth",authRouter)
