f302ffef95753be86d01d80254d9e0d77ef5e37a
Rakshit Kumar <147365205+Rakshit1804kumar@users.noreply.github.com>
2026-04-06 23:17:24 +0530

    Update auth.controllers.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -53,12 +53,13 @@ export const login=async (req,res)=>{
  
  const token=await genToken(user._id)
  
- res.cookie("token",token,{
-     httpOnly:true,
-     maxAge:7*24*60*60*1000,
-     sameSite:"None",
-     secure:false
-    })
+ res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "None",
+  path: "/",
+  maxAge: 7 * 24 * 60 * 60 * 1000
+});
  
     return res.status(200).json(user)
  
