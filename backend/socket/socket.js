4ca58e0fc1a9b1500ac710d3219dfaea56ddb34a
Rakshit Kumar <147365205+Rakshit1804kumar@users.noreply.github.com>
2026-04-06 22:35:15 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -5,11 +5,16 @@ import { Server } from "socket.io"
 let app = express()
 
 const server=http.createServer(app)
-const io=new Server(server,{
-    cors:{
-        origin:"http://localhost:5173"
-    }
-})
+const io = new Server(server, {
+  cors: {
+    origin: [
+      "http://localhost:5173",
+      "https://real-time-chat-application-azure-two.vercel.app"
+    ],
+    methods: ["GET", "POST"],
+    credentials: true
+  }
+});
  const userSocketMap ={}
  export const getReceiverSocketId=(receiver)=>{
     return userSocketMap[receiver]
