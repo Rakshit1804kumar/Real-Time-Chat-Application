99e0b980607f6c2f07162d703b4628a0b7602578
Rakshit Kumar <147365205+Rakshit1804kumar@users.noreply.github.com>
2026-04-06 23:21:11 +0530

    Update auth.controllers.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -27,7 +27,7 @@ const token=await genToken(user._id)
 res.cookie("token",token,{
     httpOnly:true,
     maxAge:7*24*60*60*1000,
-    sameSite:"Strict",
+    sameSite:"None",
     secure:true
    })
 
